cd3087bfbb0df3550abfa6518bafeb77677d83de
rahnoc <rahnoc@gmail.com>
2025-12-19 08:15:07 +0800

    Initial Commit

---
+++ RsMemPeeker/RsMemPeekerApp.swift
@@ -0,0 +1,17 @@
+//
+//  RsMemPeekerApp.swift
+//  RsMemPeeker
+//
+//  Created by Rahnoc on 2025/12/19.
+//
+
+import SwiftUI
+
+@main
+struct RsMemPeekerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
